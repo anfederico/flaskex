97599caa830c522478d647183601311f6fcdd52e
pl77 <pl77@users.noreply.github.com>
2017-04-28 00:33:59 -0400

    Change the sqlalchemy string back to sqlite
    I meant to revert the database behaviour back to sqlite before I created the pull request.

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# SQLALCHEMY_DATABASE_URI = 'sqlite:////accounts.db'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/flaskex'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////accounts.db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/flaskex'
 
 # more examples at http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls
