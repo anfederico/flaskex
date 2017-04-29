15bd6b951b8cde1f4fac30d4a02b5442d65c6eda
pl77 <pl77@users.noreply.github.com>
2017-04-29 10:31:27 -0400

    Additional Databases, PEP-8 code reformat, minor tweaks. (#1)
    - Additional Databases, PEP-8 code reformat, minor tweaks.
    - Replaced hardcoded sqlite with settings.py. 
    - Reformatted code against PEP8 suggestions. 
    - Moved 'add user' block in app.py into helpers.py for consistency. 
    - Incorporated context manager for sqlalchemy sessions into helpers.py.
    - Replaced deprecated 'TextField' in forms.py with current StringField .
    - Terminated declarations in javascript with semicolons.
    - Change the sqlalchemy string back to sqlite.
    - Removed slash from sqlite URI string.
    - An extra slash was messing up the sqlalchemy engine connection.

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,16 +1,16 @@
 function message(status) {
-	document.getElementById("spacer").innerHTML = status
+	document.getElementById("spacer").innerHTML = status;
 	$(".spacer").show().delay(2000).fadeOut()
 }
-var exit = function() {window.location.replace("/")}
+var exit = function() {window.location.replace("/")};
 var save = function() {
-	try { var theme = document.getElementsByClassName("theme selected")[0].id
-		  var c1 = theme.split('/')[0]
+	try { var theme = document.getElementsByClassName("theme selected")[0].id;
+		  var c1 = theme.split('/')[0];
 		  var c2 = theme.split('/')[1]
 		}
 	catch(e) {
-		var c1 = ""
-		var c2 = ""
+		c1 = "";
+		c2 = ""
 	}
 	$.post({
 		type: "POST",
@@ -24,16 +24,16 @@ var save = function() {
 			message(JSON.parse(response)['status'])
 		}
 	})
-}
+};
 $(document).ready(function() {
-	$(document).on("click", "#exit", exit)
-	$(document).on("click", "#save", save)
-	$(document).keypress(function(e) {if (e.which === 13) {save()}})
+	$(document).on("click", "#exit", exit);
+	$(document).on("click", "#save", save);
+	$(document).keypress(function(e) {if (e.which === 13) {save()}});
 	$(document).on("click", ".theme", function(event){
-		var id = event.target.id
-		var colors = id.split('/')
-		document.body.style.background = "linear-gradient(to right, "+colors[0]+", "+colors[1]+")"
-		$(".theme").each(function(){this.className = "theme"})
+		var id = event.target.id;
+		var colors = id.split('/');
+		document.body.style.background = "linear-gradient(to right, "+colors[0]+", "+colors[1]+")";
+		$(".theme").each(function(){this.className = "theme"});
 		event.target.className = "theme selected"
 	})
-})
+});
--- tabledef.py
+++ tabledef.py
@@ -1,23 +1,32 @@
-from sqlalchemy import *
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from settings import SQLALCHEMY_DATABASE_URI
 
-engine = create_engine('sqlite:///accounts.db', echo=True)
-Base   = declarative_base()
+Base = declarative_base()
+
+
+def db_connect():
+    """
+    Performs database connection using database settings from settings.py.
+    Returns sqlalchemy engine instance
+    """
+    return create_engine(SQLALCHEMY_DATABASE_URI)
+
 
 class User(Base):
     __tablename__ = "user"
- 
+
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True)
     password = Column(String(30))
-    email    = Column(String(50))
-    c1       = Column(String(15))
-    c2       = Column(String(15))
+    email = Column(String(50))
+    c1 = Column(String(15))
+    c2 = Column(String(15))
 
     def __repr__(self):
-        return '<User %r>' % (self.username)
-    
+        return '<User %r>' % self.username
+
+engine = db_connect()
 # Create Models
 Base.metadata.create_all(engine)
