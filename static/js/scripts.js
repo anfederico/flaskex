6146fcc6acd379f0be334bd61317072932c273d4
Rubén de Celis Hernández <rubendecelis@ingran.es>
2018-06-09 12:53:16 +0200

    Add missing semicolons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,13 @@
 function message(status, shake=false, id="") {
   if (shake) {
-    $("#"+id).effect("shake", {direction: "right", times: 2, distance: 8}, 250)
+    $("#"+id).effect("shake", {direction: "right", times: 2, distance: 8}, 250);
   } 
-  document.getElementById('feedback').innerHTML = status
-  $("#feedback").show().delay(2000).fadeOut()
+  document.getElementById('feedback').innerHTML = status;
+  $("#feedback").show().delay(2000).fadeOut();
 }
 
 function error(type) {
-  $("."+type).css("border-color", "#E14448")
+  $("."+type).css("border-color", "#E14448");
 }
 
 var login = function() {
@@ -17,16 +17,16 @@ var login = function() {
     data: {"username": $("#login-user").val(), 
            "password": $("#login-pass").val()},
     success: function(response){
-      var status = JSON.parse(response)['status']
-      if (status === 'Login successful') {location.reload()}
-      else {error('login-input')}}
-  })
+      var status = JSON.parse(response)['status'];
+      if (status === 'Login successful') {location.reload();}
+      else {error('login-input');}}
+  });
 }
 
 $(document).ready(function() {
   
-  $(document).on("click", "#login-button", login)
-  $(document).keypress(function(e) {if(e.which === 13) {login()}})
+  $(document).on("click", "#login-button", login);
+  $(document).keypress(function(e) {if(e.which === 13) {login();}});
   
   $(document).on("click", "#signup-button", function() {
     $.post({
@@ -36,12 +36,12 @@ $(document).ready(function() {
              "password": $("#signup-pass").val(), 
              "email": $("#signup-mail").val()},
       success: function(response) {
-        var status = JSON.parse(response)['status']
-        if (status === 'Signup successful') {location.reload()}
-        else {message(status, shake=true, id="signup-box")}
+        var status = JSON.parse(response)['status'];
+        if (status === 'Signup successful') {location.reload();}
+        else {message(status, shake=true, id="signup-box");}
       }
-    })
-  })
+    });
+  });
 
   $(document).on("click", "#save", function() {
     $.post({
@@ -51,8 +51,8 @@ $(document).ready(function() {
              "password": $("#settings-pass").val(), 
              "email": $("#settings-mail").val()},
       success: function(response){
-        message(JSON.parse(response)['status'])
+        message(JSON.parse(response)['status']);
       }
-    })
-  })
-})
+    });
+  });
+});
