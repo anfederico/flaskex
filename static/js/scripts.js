88094e2cf946be611501054c54770120b4d92f78
rdecelis <rdecelis@stt-systems.com>
2018-06-19 16:15:13 +0200

    Fix code style issues

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@ function message(status, shake=false, id="") {
   if (shake) {
     $("#"+id).effect("shake", {direction: "right", times: 2, distance: 8}, 250);
   } 
-  document.getElementById('feedback').innerHTML = status;
+  document.getElementById("feedback").innerHTML = status;
   $("#feedback").show().delay(2000).fadeOut();
 }
 
@@ -17,11 +17,11 @@ var login = function() {
     data: {"username": $("#login-user").val(), 
            "password": $("#login-pass").val()},
     success: function(response){
-      var status = JSON.parse(response)['status'];
-      if (status === 'Login successful') {location.reload();}
-      else {error('login-input');}}
+      var status = JSON.parse(response)["status"];
+      if (status === "Login successful") {location.reload();}
+      else {error("login-input");}}
   });
-}
+};
 
 $(document).ready(function() {
   
@@ -36,8 +36,8 @@ $(document).ready(function() {
              "password": $("#signup-pass").val(), 
              "email": $("#signup-mail").val()},
       success: function(response) {
-        var status = JSON.parse(response)['status'];
-        if (status === 'Signup successful') {location.reload();}
+        var status = JSON.parse(response)["status"];
+        if (status === "Signup successful") {location.reload();}
         else {message(status, shake=true, id="signup-box");}
       }
     });
@@ -59,12 +59,12 @@ $(document).ready(function() {
 
 // Open or Close mobile & tablet menu
 // https://github.com/jgthms/bulma/issues/856
-$('#navbar-burger-id').click(function () {
-  if($('#navbar-burger-id').hasClass('is-active')){
-    $('#navbar-burger-id').removeClass('is-active');
-    $('#navbar-menu-id').removeClass('is-active');
+$("#navbar-burger-id").click(function () {
+  if($("#navbar-burger-id").hasClass("is-active")){
+    $("#navbar-burger-id").removeClass("is-active");
+    $("#navbar-menu-id").removeClass("is-active");
   }else {
-    $('#navbar-burger-id').addClass('is-active');
-    $('#navbar-menu-id').addClass('is-active');
+    $("#navbar-burger-id").addClass("is-active");
+    $("#navbar-menu-id").addClass("is-active");
   }
 });
