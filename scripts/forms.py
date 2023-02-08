09088160b7e957ff6b0ff871505b79403f433ac9
chrisieboi <czonneveld9@hotmail.com>
2023-02-08 15:59:08 +0100

    Update forms.py
    WTForms errors #34

--- scripts/forms.py
+++ scripts/forms.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from wtforms import Form, StringField, validators
-
+from wtforms import Form
+from wtforms import (StringField)
+from wtforms.validators import InputRequired, Length
 
 class LoginForm(Form):
-    username = StringField('Username:', validators=[validators.required(), validators.Length(min=1, max=30)])
-    password = StringField('Password:', validators=[validators.required(), validators.Length(min=1, max=30)])
-    email = StringField('Email:', validators=[validators.optional(), validators.Length(min=0, max=50)])
+    username = StringField('Username:', validators=[InputRequired(), Length(min=5, max=30)])
+    password = StringField('Password:', validators=[InputRequired(), Length(min=5, max=30)])
+    email = StringField('Email:', validators=[Length(min=0, max=30)])
