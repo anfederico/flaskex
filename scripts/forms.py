32315274431a76b33bade09e789968b707f03626
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-03-28 16:03:17 +0800

    Update forms.py
    Edit1: Solved - manually changed validators.required() call to validators.DataRequired() in flask-admin -> fileadmin init.py . They distinguished them in v1.0.2

--- scripts/forms.py
+++ scripts/forms.py
@@ -4,6 +4,6 @@ from wtforms import Form, StringField, validators
 
 
 class LoginForm(Form):
-    username = StringField('Username:', validators=[validators.required(), validators.Length(min=1, max=30)])
-    password = StringField('Password:', validators=[validators.required(), validators.Length(min=1, max=30)])
+    username = StringField('Username:', validators=[validators.DataRequired(), validators.Length(min=1, max=30)])
+    password = StringField('Password:', validators=[validators.DataRequired(), validators.Length(min=1, max=30)])
     email = StringField('Email:', validators=[validators.optional(), validators.Length(min=0, max=50)])
